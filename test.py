55282813f74b883ac5132460f39148e3c2d15a40
mxyzptlk <mxyzptlk@yeah.net>
2018-09-19 14:49:46 +0800

    ud

--- test.py
+++ test.py
@@ -3,19 +3,53 @@
 # Author: Mxyzptlk
 # Date: 2018-09-18
 
-from bs4 import BeautifulSoup
-import requests
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Author: Mxyzptlk
+# Date: 2018-08-16
 
-url = 'https://www.tripadvisor.cn/Attractions-g190454-Activities-Vienna.html'
+# 1	医生
+# 2	护士
+# 3	医技
+# 4	药房
+# 5	后勤
+# 6	物资
+# 7	收费员
 
-wb_data = requests.get(url)
-soup = BeautifulSoup(wb_data.text, 'lxml')
-
-titles = soup.select('div.item.name > a')
-imgs = soup.select('img[width="200"]')
-cates = soup.select('#taplc_attraction_coverpage_attraction_0 > div > div:nth-of-type(1) > div > div > div.shelf_item_container > div:nth-of-type(4) > div.poi > div > div:nth-type(3)')
-
+import os, time
+import pyautogui as ag
+import pyperclip as pc
+from time import sleep
+from openpyxl import load_workbook
 
 
-print(titles,imgs,cates, sep='\n\n----------------------------------------------'
-                       '---------------------------------------------\n\n')
+def get_list():
+    data_sheet = load_workbook('通讯录.xlsx')
+    data = data_sheet.active
+    data_list = []
+    for i in range(2, 500):
+        if data['A' + str(i)].value is None:
+            break
+        else:
+            data_item = []
+            data_item.append(data['A' + str(i)].value)
+            data_item.append(data['B' + str(i)].value)
+            data_item.append(data['C' + str(i)].value)
+            data_item.append(data['D' + str(i)].value)
+            data_list.append(data_item)
+    return data_list
+
+
+data_sheet = load_workbook('通讯录.xlsx')
+data = data_sheet.active
+
+departmet = None
+
+for _ in range(6,328):
+    if data['a'+str(_)].value:
+        departmet = data['a'+str(_)].value
+    data['e' + str(_)] = data['b'+str(_)].value + '-' + departmet
+
+
+data_sheet.save('test2.xlsx')
+
